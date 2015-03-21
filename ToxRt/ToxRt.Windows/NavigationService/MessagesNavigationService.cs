bbf8b656c143dfcbef56115559e5730422c5e66b
samthedev <abdelkhalekdev@gmail.com>
2015-03-21 04:10:36 +0100

    Add The Inner Navigation Service Implementation

---
+++ ToxRt/ToxRt.Windows/NavigationService/MessagesNavigationService.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using ToxRt.Annotations;
+
+namespace ToxRt.NavigationService
+{
+   
+    class MessagesNavigationService : IMessagesNavigationService,INotifyPropertyChanged
+    {
+        #region Fields
+        private readonly Dictionary<string, Uri> _pagesByKey;
+        private readonly List<string> _historic;
+        private string _currentPageKey;
+        #endregion
+        #region Properties
+        public string CurrentPageKey
+        {
+            get
+            {
+                return _currentPageKey;
+            }
+
+            private set
+            {
+                if (_currentPageKey == value)
+                {
+                    return;
+                }
+
+                _currentPageKey = value;
+                OnPropertyChanged();
+            }
+        }
+        public object Parameter { get; private set; }
+        #endregion
+        #region Ctors and Methods
+        public MessagesNavigationService()
+        {
+            _pagesByKey = new Dictionary<string, Uri>();
+            _historic = new List<string>();
+        }
+        public void GoBack()
+        {
+            if (_historic.Count > 1)
+            {
+                _historic.RemoveAt(_historic.Count - 1);
+                NavigateTo(_historic.Last(), null);
+            }
+        }
+        public void NavigateTo(string pageKey)
+        {
+            NavigateTo(pageKey, null);
+        }
+
+        public virtual void NavigateTo(string pageKey, object parameter)
+        {
+            lock (_pagesByKey)
+            {
+                if (!_pagesByKey.ContainsKey(pageKey))
+                {
+                    throw new ArgumentException(string.Format("No such page: {0} ", pageKey), "pageKey");
+                }
+
+                var frame = GetDescendantFromName(Window.Current.Content, "MainFrame") as Frame;
+
+                if (frame != null)
+                {
+                    frame.Navigate(_pagesByKey[pageKey].GetType());
+                }
+                Parameter = parameter;
+                _historic.Add(pageKey);
+                CurrentPageKey = pageKey;
+            }
+        }
+
+        public void Configure(string key, Uri pageType)
+        {
+            lock (_pagesByKey)
+            {
+                if (_pagesByKey.ContainsKey(key))
+                {
+                    _pagesByKey[key] = pageType;
+                }
+                else
+                {
+                    _pagesByKey.Add(key, pageType);
+                }
+            }
+        }
+
+        private static FrameworkElement GetDescendantFromName(DependencyObject parent, string name)
+        {
+            var count = VisualTreeHelper.GetChildrenCount(parent);
+
+            if (count < 1)
+            {
+                return null;
+            }
+
+            for (var i = 0; i < count; i++)
+            {
+                var frameworkElement = VisualTreeHelper.GetChild(parent, i) as FrameworkElement;
+                if (frameworkElement != null)
+                {
+                    if (frameworkElement.Name == name)
+                    {
+                        return frameworkElement;
+                    }
+
+                    frameworkElement = GetDescendantFromName(frameworkElement, name);
+                    if (frameworkElement != null)
+                    {
+                        return frameworkElement;
+                    }
+                }
+            }
+            return null;
+        }
+        
+
+       
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
