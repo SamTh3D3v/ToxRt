10b5ae29d70185e856c106bd5f32d347d96ab908
samthedev <abdelkhalekdev@gmail.com>
2015-03-21 21:19:47 +0100

    Update UI

---
+++ ToxRt/ToxRt.Shared/Model/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToxRt.Model
+{
+    public class Message
+    {
+        #region Properties
+
+        public Friend Sender { get; set; }    
+        public String MessageText { get; set; }    
+        #endregion
+    }
+}
--- ToxRt/ToxRt.Shared/ViewModel/MessagesViewModel.cs
+++ ToxRt/ToxRt.Shared/ViewModel/MessagesViewModel.cs
@@ -1,23 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using ToxRt.Model;
 using ToxRt.NavigationService;
 
 namespace ToxRt.ViewModel
 {
-    class MessagesViewModel:ViewModelBase
+    public class MessagesViewModel:ViewModelBase
     {
         #region Fields
-
-        private IMessagesNavigationService _navigationService;
+        private IMessagesNavigationService _navigationService;    
         
         #endregion 
-        #region Properties
-        
+        #region Properties         
+        private ObservableCollection<Message> _listOnScreenMessages ;
+        private Friend _feriend;
+        private String _textMessage;
+        public ObservableCollection<Message> ListOnscreenMessages
+        {
+            get
+            {
+                return _listOnScreenMessages;
+            }
+
+            set
+            {
+                if (_listOnScreenMessages == value)
+                {
+                    return;
+                }
+
+                _listOnScreenMessages = value;
+                RaisePropertyChanged();
+            }
+        }
+        public Friend Friend
+        {
+            get
+            {
+                return _feriend;
+            }
+
+            set
+            {
+                if (_feriend == value)
+                {
+                    return;
+                }
+
+                _feriend = value;
+                RaisePropertyChanged();
+            }
+        }
+        public String TextMessage
+        {
+            get
+            {
+                return _textMessage;
+            }
+
+            set
+            {
+                if (_textMessage == value)
+                {
+                    return;
+                }
+
+                _textMessage = value;
+                RaisePropertyChanged();
+            }
+        }
         #endregion 
         #region Commands    
-        
+        private RelayCommand _sendCommand;
+        public RelayCommand SendCommand
+        {
+            get
+            {
+                return _sendCommand
+                    ?? (_sendCommand = new RelayCommand(
+                    () => ListOnscreenMessages.Add(new Message()
+                    {
+                        MessageText = TextMessage,
+                        Sender = Friend
+                    })));
+            }
+        }
         #endregion 
         #region Ctors and Methods
 
