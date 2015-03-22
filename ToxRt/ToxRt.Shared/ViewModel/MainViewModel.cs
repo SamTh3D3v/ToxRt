d896738a073f35ec8617f6f6b23e222701181105
samthedev <abdelkhalekdev@gmail.com>
2015-03-22 15:22:55 +0100

    ListFriends [Main Page]

--- ToxRt/ToxRt.Shared/ViewModel/MainViewModel.cs
+++ ToxRt/ToxRt.Shared/ViewModel/MainViewModel.cs
@@ -20,7 +20,8 @@ namespace ToxRt.ViewModel
         private IMessagesNavigationService _navigationService;
         private ObservableCollection<Friend> _listFriends;
         private Profile _currentProfile;
-        private Tox _tox;
+        private Tox _tox; 
+        // for testing purpuse only
         private ToxNode[] _nodes = new ToxNode[]
         {
             new ToxNode("192.254.75.102", 33445, new ToxKey(ToxKeyType.Public, "951C88B7E75C867418ACDB5D273821372BB5BD652740BCDF623A4FA293E75D2F")),
@@ -125,21 +126,96 @@ namespace ToxRt.ViewModel
                     }));
             }
         }
-
-        private void tox_OnFriendMessage(object sender, ToxEventArgs.FriendMessageEventArgs e)
+        private RelayCommand _addFriendCommand;
+        public RelayCommand AddFriendCommand
         {
-            //get the name associated with the friendnumber
-            string name = _tox.GetName(e.FriendNumber);
-
-            
-            //print the message to the console
-            Debug.WriteLine("<{0}> {1}", name, e.Message);
+            get
+            {
+                return _addFriendCommand
+                    ?? (_addFriendCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
         }
-
-        private void tox_OnFriendRequest(object sender, ToxEventArgs.FriendRequestEventArgs e)
+        private RelayCommand _searchFriendCommand;
+        public RelayCommand SearchFriendCommand
         {
-            //automatically accept every friend request we receive
-            _tox.AddFriendNoRequest(new ToxKey(ToxKeyType.Public, e.Id));
+            get
+            {
+                return _searchFriendCommand
+                    ?? (_searchFriendCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
+        }
+        private RelayCommand _changeStatusCommand;  
+        public RelayCommand ChangeStatusCommand
+        {
+            get
+            {
+                return _changeStatusCommand
+                    ?? (_changeStatusCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
+        }
+        private RelayCommand _addGroupeCommand;
+        public RelayCommand AddGroupeCommand
+        {
+            get
+            {
+                return _addGroupeCommand
+                    ?? (_addGroupeCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
+        }
+        private RelayCommand _loadProfileCommand;
+        public RelayCommand LoadProfileCommand
+        {
+            get
+            {
+                return _loadProfileCommand
+                    ?? (_loadProfileCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
+        }
+        private RelayCommand _settingsCommand;
+        public RelayCommand SettingsCommand
+        {
+            get
+            {
+                return _settingsCommand
+                    ?? (_settingsCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
+        }
+        private RelayCommand _creditCommand;
+        public RelayCommand CreditCommand
+        {
+            get
+            {
+                return _creditCommand
+                    ?? (_creditCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
         }
 
         #endregion
@@ -171,6 +247,20 @@ namespace ToxRt.ViewModel
                 }
             };
 
+        }
+        private void tox_OnFriendMessage(object sender, ToxEventArgs.FriendMessageEventArgs e)
+        {
+            //get the name associated with the friendnumber
+            string name = _tox.GetName(e.FriendNumber);
+
+
+            //print the message to the console
+            Debug.WriteLine("<{0}> {1}", name, e.Message);
+        }
+        private void tox_OnFriendRequest(object sender, ToxEventArgs.FriendRequestEventArgs e)
+        {
+            //automatically accept every friend request we receive
+            _tox.AddFriendNoRequest(new ToxKey(ToxKeyType.Public, e.Id));
         }
         public void Activate(object parameter)
         {
