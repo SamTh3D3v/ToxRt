5b4b9536b542b86ffd49474881c30b4aefa7acd3
samthedev <abdelkhalekdev@gmail.com>
2015-03-22 12:12:37 +0100

    Add Tox Logic

--- ToxRt/ToxRt.Shared/ViewModel/MainViewModel.cs
+++ ToxRt/ToxRt.Shared/ViewModel/MainViewModel.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using Windows.Networking.NetworkOperators;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Views;
+using SharpTox.Core;
 using ToxRt.Helpers;
 using ToxRt.Model;
 using ToxRt.NavigationService;
@@ -12,9 +15,17 @@ namespace ToxRt.ViewModel
 
     public class MainViewModel : ViewModelBase,INavigable
     {
-        #region Fields
+        #region Fields        
+        private String _localId;       
         private IMessagesNavigationService _navigationService;
         private ObservableCollection<Friend> _listFriends;
+        private Profile _currentProfile;
+        private Tox _tox;
+        private ToxNode[] _nodes = new ToxNode[]
+        {
+            new ToxNode("192.254.75.102", 33445, new ToxKey(ToxKeyType.Public, "951C88B7E75C867418ACDB5D273821372BB5BD652740BCDF623A4FA293E75D2F")),
+            new ToxNode("144.76.60.215", 33445, new ToxKey(ToxKeyType.Public, "04119E835DF3E78BACF0F84235B300546AF8B936F035185E2A8E9E0A67C8924F"))
+        };
         #endregion
         #region Properties
         public ObservableCollection<Friend> ListFriends
@@ -35,11 +46,24 @@ namespace ToxRt.ViewModel
                 RaisePropertyChanged();
             }
         }
+        public String LocalId
+        {
+            get
+            {
+                return _localId;
+            }
 
+            set
+            {
+                if (_localId == value)
+                {
+                    return;
+                }
 
-        private Profile _currentProfile  ;
-
-
+                _localId = value;
+                RaisePropertyChanged();
+            }
+        }
         public Profile CurrentProfile
         {
             get
@@ -73,6 +97,50 @@ namespace ToxRt.ViewModel
                     }));
             }
         }
+        private RelayCommand _loadedCommand;
+        public RelayCommand LoadedCommand
+        {
+            get
+            {
+                return  _loadedCommand
+                    ?? ( _loadedCommand = new RelayCommand(
+                    () =>
+                    {
+                        //Init Tox
+                        var options = new ToxOptions(true, false);
+
+                        _tox = new Tox(options);
+                        _tox.OnFriendRequest += tox_OnFriendRequest;
+                        _tox.OnFriendMessage += tox_OnFriendMessage;
+
+                        foreach (ToxNode node in _nodes)
+                            _tox.BootstrapFromNode(node);
+
+                        _tox.Name = CurrentProfile.ScreenName;
+                        _tox.StatusMessage = CurrentProfile.StatusMessage;
+                        _tox.Start();
+
+                        LocalId= _tox.Id.ToString();
+                        Debug.WriteLine("ID: {0}", LocalId);                        
+                    }));
+            }
+        }
+
+        private void tox_OnFriendMessage(object sender, ToxEventArgs.FriendMessageEventArgs e)
+        {
+            //get the name associated with the friendnumber
+            string name = _tox.GetName(e.FriendNumber);
+
+            
+            //print the message to the console
+            Debug.WriteLine("<{0}> {1}", name, e.Message);
+        }
+
+        private void tox_OnFriendRequest(object sender, ToxEventArgs.FriendRequestEventArgs e)
+        {
+            //automatically accept every friend request we receive
+            _tox.AddFriendNoRequest(new ToxKey(ToxKeyType.Public, e.Id));
+        }
 
         #endregion
         #region Ctors and Methods
@@ -104,18 +172,19 @@ namespace ToxRt.ViewModel
             };
 
         }
-        #endregion
-
         public void Activate(object parameter)
-        {            
+        {
         }
 
         public void Deactivate(object parameter)
-        {            
+        {
         }
 
         public void GoBack()
-        {            
+        {
         }
+        #endregion
+
+       
     }
 }
