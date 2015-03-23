261aa2914f7acf2cc65649d43ca15fdbf125bdde
samthedev <abdelkhalekdev@gmail.com>
2015-03-23 13:49:45 +0100

    add converter

---
+++ ToxRt/ToxRt.Shared/Converters/ListViewItemClickedConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using ToxRt.Model;
+
+namespace ToxRt.Converters
+{
+    public class ListViewItemClickedConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var args = value as ItemClickEventArgs;
+            if (args != null)
+            {
+                var friend = args.ClickedItem as Friend;
+                return friend;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
