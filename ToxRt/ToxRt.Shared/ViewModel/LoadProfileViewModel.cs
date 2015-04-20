fa66c3ea6e988034dfdeac2f019d7eb28c1c965b
samthedev <abdelkhalekdev@gmail.com>
2015-04-20 23:50:38 +0100

    Add and Setting Up LoadProfileViewModel

---
+++ ToxRt/ToxRt.Shared/ViewModel/LoadProfileViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight.Views;
+using ToxRt.Helpers;
+using ToxRt.Model;
+using ToxRt.NavigationService;
+
+namespace ToxRt.ViewModel
+{
+    public class LoadProfileViewModel : NavigableViewModelBase
+    {
+        #region Fields
+        
+        #endregion
+        #region Properties
+        
+        #endregion
+        #region Commands
+        
+        #endregion
+        #region Ctors and Methods
+        public LoadProfileViewModel(INavigationService navigationService, IDataService dataService, IMessagesNavigationService innerNavigationService)
+            : base(navigationService,dataService, innerNavigationService)
+        {
+        }
+        
+        #endregion
+    }
+}
